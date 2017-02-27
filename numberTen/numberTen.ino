802028875bb168c62b18ad20ec93ca33c21e359a
Kristian <din_dr_elvis@live.se>
2017-02-27 13:23:15 +0100

    Added comments

--- numberTen/numberTen.ino
+++ numberTen/numberTen.ino
@@ -1,32 +1,28 @@
-/*
-  This example code is in the public domain.
-*/
 
-// constants won't change. They're used here to
-// set pin numbers:
-const int potentiometerPin = 0;      // the number of the potentiometer pin
-const int steeringWheelPin = 1;
-const int echoPin = 11;
-const int trigPin = 12;
+// seting up pin numbers:
+const int gasPin = 0;      // this is the pin for the gas
+const int steeringWheelPin = 1; // this is the pin for the steering wheel
+const int echoPin = 11; // this is the pin connected to the echo pin on the ultrasound distance sensor. This pin sends a high singal. The signal length depends on the distance to the reflecting object. 
+const int trigPin = 12; // this is the pin connected to the trigger pin on the ultrasound distance sensor. Seting this pin to high will send a ultrasound pulse
 
-//Define steering pins for left motor
-const int enablePinLeft = 3;
-const int lMotorOne = 4;
+//Define steering pins for the left motor
+const int enablePinLeft = 3; //use this pin to control the motor speed
+const int lMotorOne = 4;  //use these to control rotation direction
 const int lMotorTwo = 5;
 
-//Define steering pins for right motor
-const int enablePinRight = 6;
-const int rMotorOne = 7;
+//Define steering pins for the right motor
+const int enablePinRight = 6; //use this pin to control the motor speed
+const int rMotorOne = 7;//use these to control rotation direction
 const int rMotorTwo = 8;
 
-int potentiometerRead;
+int gasRead;
 int turnPercent = 0;
 
-boolean poFor;
 boolean poLeft;
 boolean poRight;
 
 void setup() {
+  //setup correct pin mode to the corresponding pin
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   //setup left motor
@@ -38,57 +34,49 @@ void setup() {
   pinMode(lMotorOne, OUTPUT);
   pinMode(lMotorTwo, OUTPUT);
   
-  Serial.begin(9600);
+  Serial.begin(9600); //Start serial communication at 9600 baud rate (9600 bits/s).
 }
 
 void loop() {
-  //Serial.println(potentiometerRead);
-  Direction();
-  if (ping() < 10 && potentiometerRead > 0) {
-    Stop();
+  //Serial.println(gasRead);
+  Direction(); //The thrust. This is used to decide what direction the vehicle will drive
+  if (ping() < 10 && gasRead > 0) { //Get the distance measured from the ultrasound sensor and compare.
+    Stop(); //If distance is less than 10cm and direction is forward then stop the vehicle. This allows the user to put in reverse
   }
-  else {
-    gas();
+  else {  //Otherwise run the regular procedure
+    gas(); 
     forward(poLeft, poRight); 
     steer();
     prints();
   }
 }
 
-void Direction() {
-  potentiometerRead = analogRead(potentiometerPin);
-  potentiometerRead = map(potentiometerRead, 0, 1023, -255, 255);
-  
-  /*
-  if (potentiometerRead < 0) {
-    potentiometerRead = -potentiometerRead;
-    poFor = true;
-    }
-  else {
-    poFor = false;
-    }*/
+void Direction() { //Get the thrust. Although the direction is not set by this function
+  gasRead = analogRead(gasPin); //Read the value
+  gasRead = map(gasRead, 0, 1023, -255, 255); //Map to value. Max and min is chosen at 255 because of the maximum value in the function analogWrite 
+
   }
   
-void gas() {
+void gas() {  //
   //turnPercent
-  int Speed = potentiometerRead;
+  int Speed = gasRead;  
   int rightSpeed;
   int leftSpeed;
   //abs(turnPercent) <= 100
-  if (turnPercent < -50){ poLeft = 0;
+  if (turnPercent < -50){ poLeft = 0; //If 50 percent or more left turn then shift polarity.
   }
   else poLeft = 1;
-  if (turnPercent > 50) { poRight = 0;
+  if (turnPercent > 50) { poRight = 0; //If 50 percent or more right turn then shift polarity.
   }
   else poRight = 1;
 
-  if (Speed < 0) {
+  if (Speed < 0) { // If Speed is less than 0 Then reverse all polarities (make the vehicle reverse)
     poLeft = !poLeft;
     poRight = !poRight;
-    Speed = -Speed;
+    Speed = -Speed;//Make speed a positive value
   }
   
-  if (turnPercent <= 0) {
+  if (turnPercent <= 0) { //Adjusts the speed of the left 
     leftSpeed = Speed;
     rightSpeed = Speed * abs(turnPercent + 50) / 100;
   }
@@ -122,15 +110,15 @@ void forward(boolean left, boolean right) {
 }
 
 void prints(){
-  Serial.print(potentiometerRead);
+  Serial.print(gasRead);
   Serial.print("gas\t");
   Serial.print(turnPercent);
   Serial.print("ratt\t");
-  //Serial.print(potentiometerRead);
+  //Serial.print(gasRead);
   //Serial.print("vänster gas\t");
   Serial.print(poLeft);
   Serial.print("left polaritet \t");
-  //Serial.print(potentiometerRead);
+  //Serial.print(gasRead);
   //Serial.print("höger gas \t");
   Serial.print(poRight);
   Serial.println("right polaritet \t");
